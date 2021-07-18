522adeb7ff6fe42c1e95b70f6a0b7f79f6e625df
rezzaarsdy <rezaarisandy2525@gmail.com>
2021-07-18 11:18:32 +0800

    Add template

---
+++ resources/views/admin/layout.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Blank Page</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="{{ URL::asset('assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{ URL::asset('assets/plugins/fontawesome-free/css/all.min.css')}}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ URL::asset('assets/dist/css/adminlte.min.css')}}">
+</head>
+<body class="hold-transition sidebar-mini">
+
+<!-- begin wrapper -->
+
+	<div class="wrapper">
+	  <!-- Preloader -->
+	  <div class="preloader flex-column justify-content-center align-items-center">
+	    <img class="animation__shake" src="{{ URL::asset('assets/dist/img/AdminLTELogo.png')}}" alt="AdminLTELogo" height="60" width="60">
+	  </div>
+		@include('admin.partials.navbar')
+
+		@include('admin.partials.sidebar')
+
+		@yield('content')
+
+		@include('admin.partials.footer')
+
+			<!-- Control Sidebar -->
+				<aside class="control-sidebar control-sidebar-dark">
+		    <!-- Control sidebar content goes here -->
+				</aside>
+			<!-- /.control-sidebar -->
+	</div>
+
+<!-- end wrapper -->
+
+<!-- jQuery -->
+<script src="{{ URL::asset('assets/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<!-- jQuery UI 1.11.4 -->
+<script src="{{ URL::asset('assets/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="{{ URL::asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- overlayScrollbars -->
+<script src="{{ URL::asset('assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{ URL::asset('assets/dist/js/adminlte.min.js')}}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{ URL::asset('assets/dist/js/demo.js')}}"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{ URL::asset('assets/dist/js/pages/dashboard.js')}}"></script>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(
+	['namespace' => 'Admin', 'prefix' => 'admin'],
+	function(){
+		Route::get('/dashboard', 'DashboardController@index');
+	}
+);
+
+// Route::get('/dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index']);
