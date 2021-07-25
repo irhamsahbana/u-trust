79c55ff38f435b0dba78710acc9b5967443a814b
irhamsahbana <irhamsahbana@gmail.com>
2021-07-26 01:05:07 +0800

    adding columns in products table

---
+++ database/migrations/2021_07_25_165348_add_columns_in_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsInProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('id_video')->nullable()->after('type');
+            $table->text('description')->nullable()->after('filepath');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('id_video');
+            $table->dropColumn('description');
+        });
+    }
+}
