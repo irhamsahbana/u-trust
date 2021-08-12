c89de18e90fa3f195a59b2e1be97f33a864ada26
rezzaarsdy <rezaarisandy2525@gmail.com>
2021-08-13 01:13:36 +0800

    fix bug

--- resources/views/admin/series/index.blade.php
+++ resources/views/admin/series/index.blade.php
@@ -20,8 +20,8 @@
             </div>
             <div class="col-sm-6">
               <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item active">Blank Page</li>
+                <li class="breadcrumb-item"><a href="{{url('admin/dashboard')}}">Home</a></li>
+                <li class="breadcrumb-item active">Series</li>
               </ol>
             </div>
           </div>
