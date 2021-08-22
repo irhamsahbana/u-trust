74180c164c3745b0e09c7fc663bcdec533e0b946
irhamsahbana <irhamsahbana@gmail.com>
2021-08-22 15:35:23 +0800

    make seeder for users table (admin)

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Carbon\Carbon;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User;
+        $user->name = "admin";
+        $user->email = "admin@utrust.com";
+        $user->password = Hash::make('admin');
+        $user->email_verified_at = Carbon::now(); 
+        $user->save();
+    }
+}
